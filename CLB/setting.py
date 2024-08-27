5e43c9c94c10a47ecbe485e925d4d148e476c8a2
feifei0626 <13711036706@163.com>
2024-08-27 18:06:46 +0800

    first commit

---
+++ CLB/setting.py
@@ -0,0 +1,15 @@
+"""
+	Common Setting for EFattree.
+"""
+
+DISCOVERY_PERIOD = 10   # For discovering topology.
+
+MONITOR_PERIOD = 2   # For monitoring traffic.
+
+TOSHOW = True	   # For showing statistics information in terminal.
+
+enable_Flow_Entry_L4Port = False   # For including L4 port in the installing flow entries or not.
+
+MAX_CAPACITY = 10000   # Max capacity of link. (kbit/s)
+
+get_topology_delay = 30
---
+++ Hedera/setting.py
@@ -0,0 +1,13 @@
+"""
+	Common Setting of EFattree.
+"""
+
+DISCOVERY_PERIOD = 10   # For discovering topology.
+
+MONITOR_PERIOD = 2   # For monitoring traffic
+
+TOSHOW = False	   # For showing information in terminal
+
+MAX_CAPACITY = 10000   # Max capacity of link, Kbit/s
+
+get_topology_delay = 30
